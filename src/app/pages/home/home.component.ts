81ab62821ed540679de086a027a4e8b61f555daa
oscarbravo <oscarbravoarrieta@gmail.com>
2023-11-18 11:52:36 -0500

    Create a new task and delete a task

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
  import { Component, signal } from '@angular/core'
  import { CommonModule } from '@angular/common'
+ import { Task } from '../../models/task.models'
 
  @Component({
      selector: 'app-home',
@@ -10,11 +11,51 @@
  })
  export class HomeComponent {
      title = 'todoapp'
-     tasks = signal([
-         'Insertar el cli en Angular',
-         'Crear proyecto',
-         'Crear componentes'
+     tasks = signal<Task[]>([
+         {
+             id: Date.now(),
+             title: 'Crear proyecto',
+             completed: false
+         },
+         {
+             id: Date.now(),
+             title: 'Estudiar escalas',
+             completed: false
+         },
+         {
+             id: Date.now(),
+             title: 'Estudiar Angular signals',
+             completed: false
+         },
+         {
+             id: Date.now(),
+             title: 'Estudiar el estado',
+             completed: false
+         }
+
      ])
+     // -------------------------------------------------------------------------------------------
+     changeHandler(event: Event) {
+
+         const input = event.target as HTMLInputElement
+         const newTask = input.value
+         //this.tasks.update((tasks) => [...tasks, newTask])
+
+     }
+     // -------------------------------------------------------------------------------------------
+     // -------------------------------------------------------------------------------------------
+
+     deleteTask(index: number) {
+
+        console.log(index);
+
+
+         this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
+
+     }
+
+     //--------------------------------------------------------------------------------------------
+
 
 
  }
